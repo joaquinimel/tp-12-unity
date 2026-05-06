7f3596c03b326204c4fd812ccbbc6d487af87b1a
joaquinimel <49836478@est.ort.edu.ar>
2026-05-06 14:37:04 -0300

    Initial commit

---
+++ Assets/scripts/CajaSpawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaSpawn : MonoBehaviour
+{
+    int randomNumber;
+    public float StartYValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   void LocateBoxAtRandomPosition()
+    {
+         Vector3 currentPosition = transform.position;
+        float newX = 0f;
+        randomNumber = Random.Range(0,3);
+        if(randomNumber == 0){
+            newX = -2f;
+        } else if(randomNumber == 1){
+            newX = 0f;
+        } else if (randomNumber == 2){
+            newX = 2f;
+        }
+        currentPosition.x = newX;
+        currentPosition.y = StartYValue;
+        transform.position = currentPosition;
+    }
+
+
+
+}
---
+++ Assets/scripts/Cajamovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cajamovement : MonoBehaviour
+{
+    public float speed;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -speed, 0);
+    }
+   
+void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.CompareTag("Caja"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
+
+
